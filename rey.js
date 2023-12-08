2922d927ba33800393215df78154d14812f8ef06
rey arshandy <reyshaarshandy@gmail.com>
2023-12-08 23:00:04 +0700

    first commit

---
+++ rey.js
@@ -0,0 +1,35 @@
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    var href = this.getAttribute('href');
+
+    
+    if (href.startsWith('#') && document.querySelector(href)) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+    
+  });
+});
+var hamburgerMenu = document.querySelector('.hamburger-menu');
+var navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', function() {
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+});
+var hamburgerMenu = document.querySelector('.hamburger-menu');
+var sidebar = document.querySelector('.sidebar');
+
+hamburgerMenu.addEventListener('click', function() {
+    sidebar.classList.toggle('open');
+});
